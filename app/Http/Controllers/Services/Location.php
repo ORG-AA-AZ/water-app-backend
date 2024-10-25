58cb35b6aa25488c4c4d4e12efc5d590feb83e5f
abdelmajied94abusuliman <abdelmajied94abusuliman@users.noreply.github.com>
2024-10-25 15:51:41 +0000

    Fix styling issues (PHP-CS-Fixer)

--- app/Http/Controllers/Services/Location.php
+++ app/Http/Controllers/Services/Location.php
@@ -11,8 +11,7 @@ class Location
     {
         $entity = $model->value::where('mobile', $request->input('mobile'))->first();
 
-        if(!$entity)
-        {
+        if (! $entity) {
             return response()->json(['message' => 'The mobile number is not registered']);
         }
 
